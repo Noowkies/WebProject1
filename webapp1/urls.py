c5bcf89bdf689124f952e2e6dd2cb4e99742ceef
Noowkies <vboxuser@Linux.myguest.virtualbox.org>
2024-02-19 00:54:25 +0300

    BARANOV_DZ23.1

--- webapp1/urls.py
+++ webapp1/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('webapp1/', views.index, name='index'),
+    path('webapp1/firstpost', views.firstpost, name='firstpost'),
+    path('webapp1/secondpost', views.secondpost, name='secondpost'),
+    path('webapp1/home', views.home, name='home'),
+    path('webapp1/signin', views.signin, name='signin'),
 ]
--- webapp1/views.py
+++ webapp1/views.py
@@ -1,7 +1,22 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from . models import Post
 
 # Create your views here.
 
 def index(request):
     return render(request, 'index.html')
+
+def home(request):
+    return render(request, 'home.html')
+
+def firstpost(request):
+    post = Post.objects.get(id=1)
+    return render(request, 'firstpost.html', {'firstpost': post})
+
+def secondpost(request):
+    post = Post.objects.get(id=2)
+    return render(request, 'secondpost.html', {'secondpost': post})
+
+def signin(request):
+    return render(request, 'signin.html')
