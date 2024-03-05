4d504aab38a27f5ee971ae2b88e0f0372a72006d
Noowkies <vboxuser@Linux.myguest.virtualbox.org>
2024-03-05 23:49:49 +0300

    BARANOV_DZ25

--- webapp1/urls.py
+++ webapp1/urls.py
@@ -4,9 +4,11 @@ from . import views
 
 urlpatterns = [
     path('webapp1/', views.index, name='index'),
-    path('webapp1/firstpost', views.firstpost, name='firstpost'),
-    path('webapp1/secondpost', views.secondpost, name='secondpost'),
     path('webapp1/home', views.home, name='home'),
     path('webapp1/signin', views.signin, name='signin'),
-    path('webapp1/createpost', views.createpost, name='createpost')
+    path('webapp1/createpost', views.createpost, name='createpost'),
+    path('webapp1/posts', views.posts, name='posts'),
+    path('webapp1/post', views.post, name='post'),
+    path('webapp1/deletepost', views.deletepost, name='delete'),
+    path('webapp1/post/<int:id>/edit', views.editpost, name='edit'),
 ]
--- webapp1/views.py
+++ webapp1/views.py
@@ -1,5 +1,7 @@
+from math import ceil
 from django.http import HttpResponse
 from django.shortcuts import render
+from flask import redirect
 from . models import Post
 from .forms import PostForm
 
@@ -11,14 +13,6 @@ def index(request):
 def home(request):
     return render(request, 'home.html')
 
-def firstpost(request):
-    post = Post.objects.get(id=1)
-    return render(request, 'firstpost.html', {'firstpost': post})
-
-def secondpost(request):
-    post = Post.objects.get(id=2)
-    return render(request, 'secondpost.html', {'secondpost': post})
-
 def createpost(request):
     if request.method == "POST":
         new_post = PostForm(request.POST)
@@ -29,3 +23,40 @@ def createpost(request):
 
 def signin(request):
     return render(request, 'signin.html')
+
+def posts(request):
+    countpost = 10
+    page = request.GET.get('page')
+    page = int(page)
+    posts_all = Post.objects.all()
+    posts = posts_all[countpost * (page - 1):countpost * page]
+    totalpage = ceil (len(posts_all)/countpost)
+    return render(request, 'posts.html', {'posts': posts, 'next_page': page + 1, 'prev_page': page - 1, 'totalpage': totalpage})
+
+def post(request):
+    id = request.GET.get('id')
+    post = Post.objects.get(id=id)
+    return render(request, 'post.html', {'post': post, 'id': id})
+
+def deletepost(request):
+    id = request.GET.get('id')
+    try:
+        post = Post.objects.get(id=id)
+        if post:
+            post.delete()
+            return render(request, 'deletepost.html', {'remove': True})
+    except Exception:
+        return render(request, 'deletepost.html', {'remove': False})
+
+def editpost(request, id):
+    post = Post.objects.get(id=id)
+    if request.method == "POST":
+        new_post = PostForm(request.POST, instance=post)
+        if new_post.is_valid():
+            new_post.save()
+            print(1)
+        return render(request, 'post.html', {'post': post, 'id': id})
+    else:
+        new_post = PostForm(instance=post)
+        print(2)
+        return render(request, 'editpost.html', {'post_form': new_post})
